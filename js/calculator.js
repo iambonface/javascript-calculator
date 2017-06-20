56af07f971ab735426db005f0ffa11200ec19633
Bonface Mudiri <mudirib@gmail.com>
2017-06-20 23:46:24 +0300

    Update calculator.js memory values and fix input display on firefox

--- js/calculator.js
+++ js/calculator.js
@@ -22,15 +22,26 @@ function result(){
 
 function sqrt(){
 
-	var x = (Math.sqrt(document.getElementById("DisplayScreen").value)).toFixed(7)
+	var x = parseFloat((Math.sqrt(document.getElementById("DisplayScreen").value)).toFixed(7))
 
-	document.getElementById("Display").value = x
 
-	document.getElementById("DisplayScreen").value = ""
+	document.getElementById("DisplayScreen").value = x
+
+	document.getElementById("Display").value = ""
+
 
 }
 
 function clearScreen(){
+
+	document.getElementById("DisplayScreen").value = ""
+	document.getElementById("Display").value = ""
+
+}
+
+function allClear(){
+
+	memoryArr = []
 
 	document.getElementById("DisplayScreen").value = ""
 	document.getElementById("Display").value = ""
@@ -52,54 +63,55 @@ function backspace(){
 	document.getElementById("DisplayScreen").value = popped
 
 }
+
 var memoryArr = []
 var memVal
 
 function memoryAdd(){
 
-	var displayMem = document.getElementById("Display").value
+	var x = document.getElementById("DisplayScreen").value
+	var y = document.getElementById("Display").value
 
-	console.log((displayMem.split(' ')).length)
+	var xLength = (x.split('')).length
+	var yLength = (y.split('')).length
 
-	var memDisplayArr = (displayMem.split(' ')).length
+	var memLength = memoryArr.length
+	
+	if(memLength === 0 && xLength > 0){
+		document.getElementById("Display").value = "M"
+
+		memoryArr.push(document.getElementById("DisplayScreen").value)
+		document.getElementById("DisplayScreen").value = ""
+
+		console.log("arr" + memoryArr)
+	}else if(x === memoryArr.join('') && y === "M"){
+
+		document.getElementById("DisplayScreen").value = ""
+	    document.getElementById("Display").value += memoryArr.join('')
 
 
-	if(memoryArr.length > 0){
-		memoryArr = []
-
-		memoryArr.push(document.getElementById("Display").value)
-
-	}else if(memDisplayArr > 0){
-		memoryArr =[]
-
-		memoryArr.push(document.getElementById("Display").value)
-
-		document.getElementById("Display").value = (memVal)
-
-
-	}else{
-
-		memoryArr.push(document.getElementById("Display").value)
+	} else if(x === "" && yLength > 0 && memLength === 0){
+		memoryArr.push(y)
+		document.getElementById("DisplayScreen").value = y
+		document.getElementById("Display").value = "M"
 	}
 
-	memVal = memoryArr.join('')
 
-	//console.log(memVal)
-
-	document.getElementById("Display").value = (memVal)
-
-	document.getElementById("DisplayScreen").value = ""
-
+ 
 }
 
 function memoryRecall(){
-	if(memoryArr.length > 0){
-		document.getElementById("DisplayScreen").value += memoryArr.join('')
-	} else{
+
+	document.getElementById("DisplayScreen").value = ""
+	document.getElementById("DisplayScreen").value += memoryArr.join('')
+
+
+	/*if(memoryArr.length > 0){
+
+		document.getElementById("Display").value = ""
 
 		document.getElementById("DisplayScreen").value += memoryArr.join('')
-	}
-
+	}*/
 }
 
 function memoryRemove(){
@@ -115,11 +127,16 @@ function memoryRemove(){
 
 function exp(){
 
-	var x = (Math.exp(document.getElementById("DisplayScreen").value)).toFixed(2)
+	//document.getElementById("Display").value = document.getElementById("DisplayScreen").value
 
-	document.getElementById("Display").value = x
+	var x = parseFloat(Math.exp(document.getElementById("DisplayScreen").value))
+	document.getElementById("Display").value = x.toFixed(7)
 
-	document.getElementById("DisplayScreen").value = ""
+	var v = document.getElementById("Display").value
+
+	document.getElementById("DisplayScreen").value = v
+	document.getElementById("Display").value = ""
+
 
 }
 
